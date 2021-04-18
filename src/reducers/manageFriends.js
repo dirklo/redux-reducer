4ca6f4ef30957ece42a11988ad78965d6a2fd1cd
Rick <dirklo@gmail.com>
2021-04-18 15:00:56 +0200

    commit tree 0051f29326ccbb263581c466e44d287c67463c91

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            let ids = state.friends.map(friend => friend.id)
+            let indexToRemove = ids.indexOf(ids.find(id => action.id === id))
+            return {...state, friends: state.friends.filter((friend, index) => index !== indexToRemove)}
+        default:
+            return state
+    }
 }
